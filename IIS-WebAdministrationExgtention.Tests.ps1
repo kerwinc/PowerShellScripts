34cfb78c5ee17aef58ed473b3509ff776a5b7973
Kerwin Carpede <kerwinc@videojunky.co.za>
2017-03-19 10:59:35 +0200

    cleaned up content

---
+++ IIS-WebAdministrationExgtention.Tests.ps1
@@ -0,0 +1,37 @@
+Import-module WebAdministration
+Import-Module ".\IIS-Configuration-Utils.psm1" -Force
+
+# Get-SiteAppPool -SiteName "demo\demo\demo"
+
+# Write-Host "Default Web Site:" -ForegroundColor Green
+# Get-SiteAppPool -SiteName "Default Web Site"
+
+# Write-Host "Demo Site:" -ForegroundColor Green
+# Get-SiteAppPool -SiteName "Default Web Site\demo"
+
+# Write-Host "MyFancySite:" -ForegroundColor Green
+# Get-SiteAppPool -SiteName "MyFancySite"
+
+# Write-Host "MyFancySite Physical Path:" -ForegroundColor Green
+# Get-SitePhysicalPath -SiteName "MyFancySite"
+
+# Write-Host "Deploying to MyFancySite..." -ForegroundColor Green
+# Backup-WebSite -SiteName "MyFancySite" -BackupDirectory "c:\backup" -Verbose
+# Publish-WebSite -SiteName "MyFancySite" -SourceApplicationDirectoryPath "C:\ReleaseInProgress" -Verbose
+
+# Write-Host "Deploying to Demo..." -ForegroundColor Green
+# Backup-WebSite -SiteName "Default Web Site\demo" -BackupDirectory "c:\backup" -Verbose
+# Publish-WebSite -SiteName "Default Web Site\demo" -SourceApplicationDirectoryPath "C:\ReleaseInProgress" -Verbose
+
+# Write-Host "Deploying to Demo2..." -ForegroundColor Green
+# Backup-WebSite -SiteName "Default Web Site\demo2" -BackupDirectory "c:\backup" -Verbose
+# Publish-WebSite -SiteName "Default Web Site\demo2" -SourceApplicationDirectoryPath "C:\ReleaseInProgress" -Verbose:$false
+
+# New-WebSiteOrWebApplication -SiteName "MyFancySite" -Port 8081 -PhysicalPath "$env:systemdrive\inetpub\wwwroot\MyFancySite" -ApplicationPool "MyFancySite" -Force $true -Verbose
+# Backup-WebSite -SiteName "MyFancySite" -BackupDirectory "c:\backup" -Verbose 
+# Publish-WebSite -SiteName "MyFancySite" -SourceApplicationDirectoryPath "C:\ReleaseInProgress\Web" -Verbose
+
+#New-WebSiteOrWebApplication -SiteName "MyFancySite\Help" -PhysicalPath "$env:systemdrive\inetpub\wwwroot\MyFancySite-Help" -ApplicationPool "MyFancySite.Help" -Force $true -Verbose
+
+#ExtractZipFile -Zipfilename "C:\Backup\MyFancySite_18-03-17_225919.zip" -Destination "C:\Backup\MyFancySite_18-03-17_225919"
+#Restore-WebSite -SiteName "MyFancySite" -BackupZipFile "C:\Backup\MyFancySite_18-03-17_230739.zip" -Verbose
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# PowerShellScripts
+# Handy PowerShell Scripts
+
+So how many times do you write a PowerShell CmdLet for a project, forget about it a few weeks later then go search or write it from scratch again... This PowerShell repo is going to serve as my hub for all CmdLets i write for automation.
+
+***
+***IIS - Web Administration Extensions***
+
+This module has a ton of handy CmdLet wrappers for managing WebSites and WebApplications.
