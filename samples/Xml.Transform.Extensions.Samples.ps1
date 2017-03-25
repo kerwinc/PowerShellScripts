932995a98883df89ff3ee763b7b93cef5a1f414b
kerwinc <kerwinc@videojunky.co.za>
2017-03-25 18:08:09 +0200

    Transform multiple files

--- samples/Xml.Transform.Extensions.Samples.ps1
+++ samples/Xml.Transform.Extensions.Samples.ps1
@@ -1,6 +1,4 @@
-Import-module WebAdministration
-
-#Set the script location to the repo's root -SourceApplicationDirectoryPath
+#Set the script location to the repo's root
 $scriptLocation = (Get-Item -LiteralPath (Split-Path -Parent $MyInvocation.MyCommand.Path)).Parent.FullName
 Set-Location -LiteralPath $scriptLocation
 
@@ -8,5 +6,13 @@ Import-Module ".\src\XmlTransform.Extensions.psm1" -Force
 
 $ErrorActionPreference = "Stop"
 
-# Invoke-XmlTransform -XmlFilePath "C:\Backup\Web.config" -XdtFilePath "C:\Backup\Web.Release.config" -DestinationPath "C:\Backup\Web.Error.config" -TransformDllDirectory "$scriptLocation\lib"  -Verbose
 # Invoke-XmlTransform -XmlFilePath "C:\Backup\Web.config" -XdtFilePath "C:\Backup\Web.Release.config" -TransformDllDirectory "$scriptLocation\lib" -Verbose
+
+#Test a bunch of transforms at once
+$config = "C:\Backup\Web.config"
+$transformFiles = Get-ChildItem -Path "C:\Backup\*.config" -Exclude "web.config"
+foreach ($xdtFile in $transformFiles ) {
+  Write-Host "Transforming $config with $($xdtFile.Name):"
+  $xdtFilePath = 
+  Invoke-XmlTransform -XmlFilePath "C:\Backup\Web.config" -XdtFilePath "$($xdtFile.FullName)" -DestinationPath "C:\Backup\output\$($xdtFile.Name)" -TransformDllDirectory "$scriptLocation\lib"
+}
